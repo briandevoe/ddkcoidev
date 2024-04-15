00b728125eab16bbf9083ad72b40deaf0586cc15
briandevoe <bdevoe@brandeis.edu>
2024-04-15 19:48:20 -0400

    package documentation - bd

--- R/ddkcoidev.R
+++
@@ -1,5 +0,0 @@
-#' @name ddkcoidev
-#' @title ddkcoidev
-#' @author brian devoe
-#' @description load table from SQL database load table from SQL database load table from SQL databaseload table from SQL databaseload table from SQL databaseload table from SQL database
-#' load table from SQL databaseload table from SQL databaseload table from SQL database
