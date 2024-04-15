9b559f8afd6facbf8fbcec70c5f79c30ebc288ae
briandevoe <bdevoe@brandeis.edu>
2024-04-15 19:27:45 -0400

    adding some documentation - bd

---
+++ R/ddkcoidev.R
@@ -0,0 +1,5 @@
+#' @name ddkcoidev
+#' @title ddkcoidev
+#' @author brian devoe
+#' @description load table from SQL database load table from SQL database load table from SQL databaseload table from SQL databaseload table from SQL databaseload table from SQL database
+#' load table from SQL databaseload table from SQL databaseload table from SQL database
