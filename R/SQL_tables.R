da15f1e032228d5196476fcdb84ca285a37962a2
briandevoe <bdevoe@brandeis.edu>
2024-04-16 02:40:04 -0400

    bd

--- R/SQL_tables.R
+++ R/SQL_tables.R
@@ -1,11 +1,11 @@
 #' @name SQL_tables
 #' @title SQL_tables
 #' @author brian devoe
+#' @export
 #'
 #' @description
 #' Call function to list available tables in COI SQL database
 #'
-#' @param none
 
 # libraries
 library(RMariaDB)
