f6c002ff5c312875b17bb6554f68f618bf150c20
briandevoe <bdevoe@brandeis.edu>
2024-04-15 20:26:50 -0400

    package documentation - bd

--- R/ddkcoidev-package.R
+++ R/ddkcoidev-package.R
@@ -1,8 +1,32 @@
-#' @keywords internal
-"_PACKAGE"
+# heading ------------------------------------------------------------------------------------------
 
+#' @keywords internal
+#' @title ddkcoidev - Data Diversity Kids COI Development Library
+#' @author Brian DeVoe
+#' @seealso git repo:  \url{https://github.com/briandevoe/ddkcoidev}
+
+# body ---------------------------------------------------------------------------------------------
+
+# NOTES:
+# ~skip 4 lines for new paragraph
+
+#' @description
+#' ddkcoidev is an internal library to be used by our Data and Development team at Data Diversity Kids
+#' for the development of the Child Opportunity Index (COI) and other related projects.
+
+
+#' @details
+#' More detailed documentation here. We can also link to vignettes, describe main functions, describe
+#' built in data, use examples, and provide references to other documentation.
+
+
+
+
+
+# end ----------------------------------------------------------------------------------------------
+
+"_PACKAGE"
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
 
-#' @description Here is an example of a package DESCRIPTION file:
